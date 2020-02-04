4ea8f114df33b14b6382ac079749f9ead2a57a54
Pelayo Martinez <pmartinezcapela@gmail.com>
2020-02-04 04:16:22 +0100

    Pagina principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LoginComponent } from './login/login.component';
+import { NoFoundComponent } from './no-found/no-found.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SharedService } from './service/shared.service';
+import { SidebarService } from './service/sidebar.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NoFoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    HeaderComponent,
+    NavbarComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [SharedService, SidebarService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
