bf825629d6ccb2224ae9d76583255973d962cbbd
Pelayo Martinez <pmartinezcapela@gmail.com>
2020-07-02 04:06:14 +0200

    Puesto el config correcto

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,4 +1,4 @@
-export const URL_SERVICIOS = 'http://localhost:3000';
-export const URL_APP = 'http://localhost:4200';
-// export const URL_SERVICIOS = 'https://api-rest-uo250985.herokuapp.com';
-// export const URL_APP = 'https://proyecto-tfg-1582773697712.web.app';
+// export const URL_SERVICIOS = 'http://localhost:3000';
+// export const URL_APP = 'http://localhost:4200';
+export const URL_SERVICIOS = 'https://api-rest-uo250985.herokuapp.com';
+export const URL_APP = 'https://proyecto-tfg-1582773697712.web.app';
